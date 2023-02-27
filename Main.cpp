bf50c397f0a7c56e6914c119fe21592ccf8d2462
ElectricGtr <114348027+MakaronynaZavtrak@users.noreply.github.com>
2023-02-27 12:12:31 +0500

    Create Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <math.h>
+#include <string>
+#include "Header.h"
+#include "DataStudents.cpp"
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "RUS");
+	DataBase students[20];
+	
+	return 0;
+}
