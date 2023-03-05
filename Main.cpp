93fe94edece65ce6b2c9e753b58df664a79eba0a
ElectricGtr <114348027+MakaronynaZavtrak@users.noreply.github.com>
2023-03-05 22:40:02 +0500

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,12 +1,35 @@
 #include <iostream>
-#include <math.h>
+#include <fstream>
+#include <cstdio>
 #include <string>
+#include <vector>
 #include "Header.h"
-#include "DataStudents.cpp"
 using namespace std;
-int main() {
+//перегрузка оператора cout для объектов структур
+ostream& operator<<(ostream& os, const StudentsDB& student)
+{
+	os << student.lastName << " " << student.name << " " << student.father << " " << student.groupNumber << " " << student.studNumber << endl;
+	return os;
+};
+
+int main()
+{
 	setlocale(LC_ALL, "RUS");
-	DataBase students[20];
+	string path = "students_db.txt";
+	ifstream File;
+	ifstream& copy = File;
+
+
+	//проверка открытия файла
 	
+
+	int amounOfAllStudents = countStudents(File, path);
+	print(amounOfAllStudents);
+
+	StudentsDB* allStudents = new StudentsDB[amounOfAllStudents];//создаём массив структур абсолютно всех студентов
+	allStudents = recordData(amounOfAllStudents, allStudents, File, path);
+	for (int i = 0; i < amounOfAllStudents; i++) {
+		cout << allStudents[i] << endl;
+	};
 	return 0;
-}
+};
